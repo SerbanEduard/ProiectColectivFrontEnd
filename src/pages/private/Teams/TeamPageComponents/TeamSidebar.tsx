db39dd39918332d5ca15d97615b0500c9f756600
Skybby15 <savaclaudiu42@gmail.com>
2025-11-15 18:27:00 +0200

    Mid commit

--- src/pages/private/Teams/TeamPageComponents/TeamSidebar.tsx
+++ src/pages/private/Teams/TeamPageComponents/TeamSidebar.tsx
@@ -1,4 +1,5 @@
 // components/MySidebar.tsx
+import { CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
 import {
   Sidebar,
   SidebarContent,
@@ -11,7 +12,9 @@ import {
   SidebarHeader,
   SidebarFooter,
 } from "@/components/ui/sidebar";
+import { Collapsible } from "@radix-ui/react-collapsible";
 import { Home, Settings, User } from "lucide-react";
+import { useState } from "react";
 
 export function MySidebar() {
   const items = [
@@ -20,29 +23,131 @@ export function MySidebar() {
     { title: "Settings", icon: Settings, url: "/settings" },
   ];
 
+  const subItems = [
+    { title: "Files", icon: Home },
+    { title: "Events", icon: Home}
+  ]
+
+  const [openMap, setOpenMap] = useState<Record<string, boolean>>({
+    Home: false,
+    Profile: false,
+    Settings: false,
+  });
+
   return (
     <Sidebar variant="floating">
         <SidebarHeader className="border">
-            Heeyooo
+            Team name here
         </SidebarHeader>
         <SidebarContent>
+          <Collapsible>
             <SidebarGroup>
-            <SidebarGroupLabel>Main Menu</SidebarGroupLabel>
-            <SidebarGroupContent>
-                <SidebarMenu>
-                {items.map((item) => (
-                    <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild>
-                        <a href={item.url} className="flex items-center gap-2">
-                        <item.icon size={16} />
-                        {item.title}
-                        </a>
-                    </SidebarMenuButton>
-                    </SidebarMenuItem>
-                ))}
-                </SidebarMenu>
-            </SidebarGroupContent>
+              <CollapsibleTrigger>
+                <SidebarGroupLabel className="font-bold text-sm">Chat Rooms</SidebarGroupLabel>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+              <SidebarGroupContent>
+                  <SidebarMenu>
+                  {items.map((item) => (
+                      <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                          <a href={item.url} className="flex items-center gap-2">
+                          <item.icon size={16} />
+                          {item.title}
+                          </a>
+                      </SidebarMenuButton>
+                      </SidebarMenuItem>
+                  ))}
+                  </SidebarMenu>
+              </SidebarGroupContent>
+              </CollapsibleContent>
             </SidebarGroup>
+          </Collapsible>
+          <Collapsible>
+            <SidebarGroup>
+              <CollapsibleTrigger>
+                <SidebarGroupLabel className="font-bold text-sm">Voice Rooms</SidebarGroupLabel>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+              <SidebarGroupContent>
+                  <SidebarMenu>
+                  {items.map((item) => (
+                      <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                          <a href={item.url} className="flex items-center gap-2">
+                          <item.icon size={16} />
+                          {item.title}
+                          </a>
+                      </SidebarMenuButton>
+                      </SidebarMenuItem>
+                  ))}
+                  </SidebarMenu>
+              </SidebarGroupContent>
+              </CollapsibleContent>
+            </SidebarGroup>
+          </Collapsible>
+          <Collapsible>
+            <SidebarGroup>
+              <CollapsibleTrigger>
+                <SidebarGroupLabel className="font-bold text-sm">Chat Rooms</SidebarGroupLabel>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+              <SidebarGroupContent>
+                  <SidebarMenu>
+                  {items.map((item) => (
+                      <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                          <a href={item.url} className="flex items-center gap-2">
+                          <item.icon size={16} />
+                          {item.title}
+                          </a>
+                      </SidebarMenuButton>
+                      </SidebarMenuItem>
+                  ))}
+                  </SidebarMenu>
+              </SidebarGroupContent>
+              </CollapsibleContent>
+            </SidebarGroup>
+          </Collapsible>
+          <Collapsible>
+            <SidebarGroup>
+              <CollapsibleTrigger>
+                <SidebarGroupLabel className="font-bold text-sm">Chat Rooms</SidebarGroupLabel>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+              <SidebarGroupContent>
+                  <SidebarMenu>
+                  {items.map((item) => (
+                      <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                          <a href={item.url} className="flex items-center gap-2">
+                          <item.icon size={16} />
+                          {item.title}
+                          </a>
+                      </SidebarMenuButton>
+                      </SidebarMenuItem>
+                  ))}
+                  </SidebarMenu>
+              </SidebarGroupContent>
+              </CollapsibleContent>
+            </SidebarGroup>
+          </Collapsible>
+          <SidebarGroup>
+            <SidebarGroupContent>
+                  <SidebarMenu>
+                  {subItems.map((item) => (
+                      <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <div>
+                          <item.icon size={16} />
+                          {item.title}
+                        </div>
+                      </SidebarMenuButton>
+                      </SidebarMenuItem>
+                  ))}
+                  </SidebarMenu>
+              </SidebarGroupContent>
+          </SidebarGroup>
         </SidebarContent>
         <SidebarFooter className="border">
             Heyooo
